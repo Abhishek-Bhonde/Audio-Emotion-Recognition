0703265f0aa8861d635b3bf7550207a0e8f8d965
Abhishek Bhonde <121537842+Abhishek-Bhonde@users.noreply.github.com>
2024-01-07 11:43:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
 import numpy as np
 from keras.models import load_model
-from flask import Flask, render_template, request, redirect, url_for,jsonify
+from flask import Flask, render_template, request
 import librosa
-import python_speech_features
 
 model = load_model('audio.h5')
 
 emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Neutral', 'Pleasant Surprise', 'Sad']
 
-app = Flask(__name__, template_folder="template", static_folder="static")
-
+app = Flask(__name__, template_folder="templates", static_folder="static")
 
 @app.route('/')
 def index():
@@ -18,32 +16,28 @@ def index():
 @app.route('/recognize-emotion', methods=['POST'])
 def recognize_emotion():
     if 'audio' not in request.files:
-        return jsonify({'error': 'No audio file provided'})
+        return render_template('index.html', prediction='', error='No audio file provided')
 
     audio_file = request.files['audio']
 
     if audio_file.filename == '':
-        return jsonify({'error': 'No selected audio file'})
+        return render_template('index.html', prediction='', error='No selected audio file')
 
     try:
-        
         y, sr = librosa.load(audio_file, duration=3, offset=0.5)
         mfcc = np.mean(librosa.feature.mfcc(y=y, sr=sr, n_mfcc=40).T, axis=0)
         mfcc = np.expand_dims(mfcc, axis=0)
         mfcc = np.expand_dims(mfcc, axis=2)
 
-        
         prediction = model.predict(mfcc)
         predicted_label = np.argmax(prediction)
         predicted_emotion = emotions[predicted_label]
 
-        return jsonify({'emotion': predicted_emotion})
+        return render_template('index.html', prediction=predicted_emotion, error='')
 
     except Exception as e:
-        return jsonify({'error': str(e)})
-
+        return render_template('index.html', prediction='', error=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
-  
+
